7869b06985c53c9d81ba4a7f34c39633c267827b
Alcatrez97 <47591066+Alcatrez97@users.noreply.github.com>
2020-09-05 13:37:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     'secretKey': '123-432-452-5474-304',
-    'mongoUrl': 'mongodb://mongodb+srv://root:toor@alca1.9aycb.mongodb.net/<dbname>?retryWrites=true&w=majority:27017/conFusion',
+    'mongoUrl': 'Your Mongo Url',
     "facebook": {
-        clientId: "591172048221363",
-        clientSecret: "38d1273669ee4c27d61327b07d11bee6"
+        clientId: "Facebook client id fo o-auth",
+        clientSecret: "client secret"
     }
 }
